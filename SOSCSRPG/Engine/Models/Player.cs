48f5f4963b1001e7e75ae7fb06cc814bf286dacf
Amadurr <amarduranovic93@gmail.com>
2019-02-18 21:05:26 +0100

    Added Engine Project and created Player Class.

---
+++ SOSCSRPG/Engine/Models/Player.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Engine.Models
+{
+    class Player
+    {
+        string name { get; set; }
+        string characterClass { get; set; }
+        int hitPoints { get; set; }
+        int experiencePoints { get; set; }
+        int level { get; set; }
+        int gold { get; set; }
+    }
+}
