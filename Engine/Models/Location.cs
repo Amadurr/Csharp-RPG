cd793d16bc8235b480694ef3213c05b7cccfa051
Amadurr <amarduranovic93@gmail.com>
2019-02-18 22:43:20 +0100

    Added properties for the Location class.

--- Engine/Models/Location.cs
+++ Engine/Models/Location.cs
@@ -8,5 +8,10 @@ namespace Engine.Models
 {
     public class Location
     {
+        public int XCoordinate { get; set; }
+        public int YCoordinate { get; set; }
+        public string LocationName { get; set; }
+        public string LocationDescription { get; set; }
+        public string ImageName { get; set; }
     }
 }
